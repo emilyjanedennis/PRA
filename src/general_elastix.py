85c256023f09f7e355347b5e76bda27e88a78abc
emilyjanedennis <emilyjanedennis@gmail.com>
2023-10-17 09:49:16 -0400

    edits to general transformix and elastix for easier use

--- src/general_elastix.py
+++ src/general_elastix.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -31,15 +30,19 @@ if __name__ == "__main__":
 
 	try:
 		param_fld=str(sys.argv[1])
-		if ~os.path.isdir(param_fld):
+		if not os.path.isdir(param_fld):
 			print("the parameter folder {} does not exist, please check your path".format(param_fld))
+		else: 
+			print("using param_fld {}".format(param_fld))
+	
 	except:
 		print("this function requires four inputs, the first must be a string containing a full path to the param_fld, e.g. '/scratch/ejdennis/parameter_folder_a2b')")
 
 	try:
-		mvtiffs = eval(sys.argv[2])
+		mvtiffs = eval(str(sys.argv[2]))
+		print(mvtiffs)
 		if type(mvtifffs)!=list:
-			print('the second input mvtiffs should be a list structured as ["/scratch/ejdennis/files_to_align/brain1.tiff","/scratch/ejdennis/files_to_align/brain2.tiff"] or ["brain1.tiff","brain2.tiff"] if using src (optional input 4)')
+			print('the second input mvtiffs should be a list structured as ["/scratch/ejdennis/files_to_align/brain1.tiff","/scratch/ejdennis/files_to_align/brain2.tiff"] or ["brain1.tiff","brain2.tiff"] if using src (optional input 4). \n you entered {} which is a {}'.format(mvtiffs,type(mvtiffs)))
 		else:
 			for val in mvtiffs:
 				if "tif" not in val[-4:]:
@@ -67,6 +70,7 @@ if __name__ == "__main__":
 	if len(sys.argv) > 5:
 		try:
 			mult=float(sys.argv[5])
+			src=""
 		except:
 			print("a fifth argument (mult) was provided, but it could not be converted to a float, USING DEFAULT VALUE OF 1.4")
 	else:
@@ -78,20 +82,35 @@ if __name__ == "__main__":
 			if os.path.isdir(str(sys.argv[6])):
 				src=str(sys.argv[6])
 			else:
-				print("a sixth argument (src) was provided as {} but it is not a directory, proceeding with no src")
-
+				src=""
+				print("a sixth argument (src) was provided as {} but it is not a directory, proceeding with no src".format(sys.argv[6]))
+		except:
+			src=""
+			print('no sixth argument, no src used')
 	print("printing all inputs: param_fld, mvtiffs, fxtiff, output_dir, mult, src".format(param_fld, mvtiffs, fxtiff, output_dir, mult, src))
 
 	for pairnum in np.arange(0,len(mvtiffs)):
 		mvtiff = mvtiffs[pairnum]
+		mvtiff_base=os.path.basename(mvtiff).split('.')[0]
+		fxtiff_base=os.path.basename(fxtiff).split('.')[0]
 		print(fxtiff)
+		print('mvtiff')
 		print(mvtiff)
+		print('mvtiff_base')
+		print(mvtiff_base)
 		fx = os.path.join(src,fxtiff)
 		mv = os.path.join(src,mvtiff)
-		outputfilename = os.path.join(output_dir,"enlarged_tiffs/{}_for_{}.tif".format(mvtiff,fxtiff))
-		print(outputfilename)
-		outputdirectory = os.path.join(output_dir,"output_dirs/{}_to_{}".format(mvtiff,fxtiff))
-
+		output_filename = os.path.join(output_dir,"enlarged_tiffs/{}_for_{}.tif".format(mvtiff_base,fxtiff_base))
+		print('output_filename')
+		print(output_filename)
+		print('output_directory')
+		output_directory = os.path.join(output_dir,"{}_to_{}".format(mvtiff_base,fxtiff_base))
+		print(output_directory)
+		if not os.path.isdir(os.path.join(output_dir,"enlarged_tiffs")):
+			os.mkdir(os.path.join(output_dir,"enlarged_tiffs"))
+		if not os.path.isdir(output_directory):
+			os.mkdir(output_directory)
+		
 		# need to make moving larger (~140% seems to work well?) to transform to fixed
 		moving = tif.imread(mv)
 		fixed = tif.imread(fx)
@@ -103,12 +122,12 @@ if __name__ == "__main__":
 
 		# saved out volume
 		print("\nsaving zoomed volume...")
-		tif.imsave(outputfilename,moving_for_fixed.astype("uint16"))
+		tif.imsave(output_filename,moving_for_fixed.astype("uint16"))
 
 
-		if not os.path.exists(outputdirectory):
-	    		os.mkdir(outputdirectory)
+		if not os.path.exists(output_directory):
+	    		os.mkdir(output_directory)
 
 		params = [os.path.join(param_fld, xx) for xx in os.listdir(param_fld)]
 		params.sort()
-		e_out, transformfiles = elastix_command_line_call(fx, outputfilename, outputdirectory, params)
+		e_out, transformfiles = elastix_command_line_call(fx, output_filename, output_directory, params)
---
+++ src/general_elastix.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+#
+#SBATCH -c 11                      # number of cores
+#SBATCH -t 240                  # time (minutes)
+#SBATCH -o logs/id_inj_%j_%a.out        # STDOUT #add _%a to see each array job
+#SBATCH -e logs/id_inj_%j_%a.err        # STDERR #add _%a to see each array job
+#SBATCH --contiguous #used to try and get cpu mem to be contigous
+#SBATCH --mem 120000 #120 gbs
+
+module load anacondapy/2020.11
+module load elastix/4.8
+. activate lightsheet
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+xvfb-run python general_elastix.py $1 $2 $3 $4 $5 $6
+
+
