269e6e5bb56056e2725dc689f1fcc11bcee8b0ba
Dennis <emilyjanedennis@gmail.com>
2026-02-17 13:46:22 -0500

    updating

---
+++ src/atlas_elastix_affine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ARG1=${1:-''}
+ARG2=${2:-''}
+ARG3=${3:-''}
+#ARG4=${4:-''}
+
+echo "mv: $ARG1"
+echo "fx: $ARG2"
+echo "output dir (optional): $ARG3"
+#echo "annotation volume (optional): $ARG4"
+
+~/miniforge3/envs/cm2/bin/python elastix_fx_to_mv_affineONLY.py "$ARG1" "$ARG2" "$ARG3" "$ARG4"
